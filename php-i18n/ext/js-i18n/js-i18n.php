a3133d88b3fd82d68a1219b65b3372a30dcbeab8
Paolo Montalto <montypablo@gmail.com>
2017-02-28 15:31:07 +0100

    Improved js-i18n Javascript plugin with async methods returning a promise

--- php-i18n/ext/js-i18n/js-i18n.php
+++ php-i18n/ext/js-i18n/js-i18n.php
@@ -16,6 +16,10 @@
  * along with php-i18n.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+ 
+ /***
+  * js-i18n v1.1
+  */
 
 $relativePath = str_replace($_SERVER["DOCUMENT_ROOT"], "", $phpi18n_PATH);
 ?>
@@ -24,6 +28,7 @@ $relativePath = str_replace($_SERVER["DOCUMENT_ROOT"], "", $phpi18n_PATH);
 
 /**
  * @param key the key identifying the localized string
+ * @return the localized string
  */
 function getString(key){
 	var str = $.ajax({
@@ -37,7 +42,26 @@ function getString(key){
 
 /**
  * @param key the key identifying the localized string
+ * @return a Promise for the localized string
+ */
+function getStringAsync(key){
+	return new Promise(function(resolve, reject) {
+		$.post({
+			url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
+			data: {key: key},
+			success: function(data) {
+				resolve(data);
+			}
+		}).fail(function() {
+			reject(null);
+		});
+	});
+}
+
+/**
+ * @param key the key identifying the localized string
  * @param optional args a comma separated list of arguments
+ * @return the localized formatted string
  */
 function getFormattedString(){
 	var key = arguments[0];
@@ -57,7 +81,37 @@ function getFormattedString(){
 	return str;
 }
 
+/**
+ * @param key the key identifying the localized string
+ * @param optional args a comma separated list of arguments
+ * @return a Promise for the localized formatted string
+ */
+function getFormattedStringAsync(){
+	var key = arguments[0];
+	var argsArr = [].splice.call(arguments,0);
+	var args="";
+	for (var i = 1; i < argsArr.length; i++){
+		args = args + argsArr[i];
+		if (i!=argsArr.length-1)
+			args = args + ",";
+	}
+		
+	return new Promise(function(resolve, reject) {
+		$.post({
+			url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
+			data: {key: key, args: args},
+			success: function(data) {
+				resolve(data);
+			}
+		}).fail(function() {
+			reject(null);
+		});
+	});
+}
 
+/**
+ * @return the currrent language
+ */
 function getCurrentLanguage() {
 	var str = $.ajax({
 		type: "post",
@@ -66,6 +120,23 @@ function getCurrentLanguage() {
 		async: false
 		}).responseText;
 	return str;
+}
+
+/**
+ * @return a Promise for the current language
+ */
+function getCurrentLanguageAsync() {
+	return new Promise(function(resolve, reject) {
+		$.post({
+			url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
+			data: {cmd: "getCurrentLanguage"},
+			success: function(data) {
+				resolve(data);
+			}
+		}).fail(function() {
+			reject(null);
+		});
+	});
 }
 //-->
 </script>
