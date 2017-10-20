9dc452181fd282289ccdcf12ad2fe1eb92eeaf97
Paolo Montalto <p.montalto@twomenstudio.com>
2017-10-20 10:12:57 +0200

    js-i18n extension: fixed getStringAsync and getFormattedStringAsync switching from $.post to $.ajax (with async: false). Fixes Issue #1

--- php-i18n/ext/js-i18n/js-i18n.php
+++ php-i18n/ext/js-i18n/js-i18n.php
@@ -18,125 +18,137 @@
  */
  
  /***
-  * js-i18n v1.1
+  * js-i18n v1.2
   */
 
 $relativePath = str_replace($_SERVER["DOCUMENT_ROOT"], "", $phpi18n_PATH);
 ?>
 <script type="text/javascript">
-<!--
+    <!--
 
-/**
- * @param key the key identifying the localized string
- * @return the localized string
- */
-function getString(key){
-	var str = $.ajax({
-		type: "post",
-		url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
-		data: {key: key},
-		async: false
-		}).responseText;
-	return str;
-}
+    /**
+     * @param key the key identifying the localized string
+     * @return the localized string
+     */
+    function getString(key){
+        var str = $.ajax({
+            type: "post",
+            url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
+            data: {key: key},
+            async: false
+        }).responseText;
+        return str;
+    }
 
-/**
- * @param key the key identifying the localized string
- * @return a Promise for the localized string
- */
-function getStringAsync(key){
-	return new Promise(function(resolve, reject) {
-		$.post({
-			url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
-			data: {key: key},
-			success: function(data) {
-				resolve(data);
-			}
-		}).fail(function() {
-			reject(null);
-		});
-	});
-}
+    /**
+     * @param key the key identifying the localized string
+     * @return a Promise for the localized string
+     */
+    function getStringAsync(key){
+        return new Promise(function(resolve, reject) {
+            try {
+                $.ajax({
+                    type: 'POST',
+                    url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
+                    data: {key: key},
+                    success: function (data) {
+                        resolve(data);
+                    },
+                    aync: false
+                }).fail(function () {
+                    reject(null);
+                });
+            } catch(e) {
+                console.log(e.message);
+            }
+        });
+    }
 
-/**
- * @param key the key identifying the localized string
- * @param optional args a comma separated list of arguments
- * @return the localized formatted string
- */
-function getFormattedString(){
-	var key = arguments[0];
-	var argsArr = [].splice.call(arguments,0);
-	var args="";
-	for (var i = 1; i < argsArr.length; i++){
-		args = args + argsArr[i];
-		if (i!=argsArr.length-1)
-			args = args + ",";
-	}
-	var str = $.ajax({
-		type: "post",
-		url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
-		data: {key: key, args: args},
-		async: false
-		}).responseText;
-	return str;
-}
+    /**
+     * @param key the key identifying the localized string
+     * @param optional args a comma separated list of arguments
+     * @return the localized formatted string
+     */
+    function getFormattedString(){
+        var key = arguments[0];
+        var argsArr = [].splice.call(arguments,0);
+        var args="";
+        for (var i = 1; i < argsArr.length; i++){
+            args = args + argsArr[i];
+            if (i!=argsArr.length-1)
+                args = args + ",";
+        }
+        var str = $.ajax({
+            type: "post",
+            url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
+            data: {key: key, args: args},
+            async: false
+        }).responseText;
+        return str;
+    }
 
-/**
- * @param key the key identifying the localized string
- * @param optional args a comma separated list of arguments
- * @return a Promise for the localized formatted string
- */
-function getFormattedStringAsync(){
-	var key = arguments[0];
-	var argsArr = [].splice.call(arguments,0);
-	var args="";
-	for (var i = 1; i < argsArr.length; i++){
-		args = args + argsArr[i];
-		if (i!=argsArr.length-1)
-			args = args + ",";
-	}
-		
-	return new Promise(function(resolve, reject) {
-		$.post({
-			url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
-			data: {key: key, args: args},
-			success: function(data) {
-				resolve(data);
-			}
-		}).fail(function() {
-			reject(null);
-		});
-	});
-}
+    /**
+     * @param key the key identifying the localized string
+     * @param optional args a comma separated list of arguments
+     * @return a Promise for the localized formatted string
+     */
+    function getFormattedStringAsync(){
+        var key = arguments[0];
+        var argsArr = [].splice.call(arguments,0);
+        var args="";
+        for (var i = 1; i < argsArr.length; i++){
+            args = args + argsArr[i];
+            if (i!=argsArr.length-1)
+                args = args + ",";
+        }
 
-/**
- * @return the currrent language
- */
-function getCurrentLanguage() {
-	var str = $.ajax({
-		type: "post",
-		url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
-		data: {cmd: "getCurrentLanguage"},
-		async: false
-		}).responseText;
-	return str;
-}
+        return new Promise(function(resolve, reject) {
+            try {
+                $.ajax({
+                    type: 'POST',
+                    url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
+                    data: {key: key, args: args},
+                    success: function (data) {
+                        resolve(data);
+                    },
+                    aync: false
+                }).fail(function () {
+                    reject(null);
+                });
+            } catch(e) {
+                console.log(e.message);
+            }
+        });
+    }
 
-/**
- * @return a Promise for the current language
- */
-function getCurrentLanguageAsync() {
-	return new Promise(function(resolve, reject) {
-		$.post({
-			url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
-			data: {cmd: "getCurrentLanguage"},
-			success: function(data) {
-				resolve(data);
-			}
-		}).fail(function() {
-			reject(null);
-		});
-	});
-}
-//-->
+    /**
+     * @return the currrent language
+     */
+    function getCurrentLanguage() {
+        var str = $.ajax({
+            type: "post",
+            url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
+            data: {cmd: "getCurrentLanguage"},
+            async: false
+        }).responseText;
+        return str;
+    }
+
+    /**
+     * @return a Promise for the current language
+     */
+    function getCurrentLanguageAsync() {
+        return new Promise(function(resolve, reject) {
+            $.post({
+                url: "<?php print $relativePath; ?>ext/js-i18n/getString.php",
+                data: {cmd: "getCurrentLanguage"},
+                success: function(data) {
+                    resolve(data);
+                }
+            }).fail(function() {
+                reject(null);
+            });
+        });
+    }
+    //-->
 </script>
